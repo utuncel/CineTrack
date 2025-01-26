37f15d88f4c6dc4c31db2b6853e3161fbf7ca8b3
utuncel <umut.2000@t-online.de>
2025-01-26 21:43:25 +0100

    created comments for ApiCinematic

--- src/CineTrack/src/main/java/org/com/model/domain/ApiCinematic.java
+++ src/CineTrack/src/main/java/org/com/model/domain/ApiCinematic.java
@@ -3,6 +3,10 @@ package org.com.model.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents cinematic data retrieved from an external API. This class is mapped to JSON fields
+ * using Jackson annotations.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ApiCinematic {
 
@@ -36,46 +40,101 @@ public class ApiCinematic {
   @JsonProperty("imdbVotes")
   private String imdbVotes;
 
+  /**
+   * Gets the IMDb votes count for the cinematic.
+   *
+   * @return The IMDb votes count as a string.
+   */
   public String getImdbVotes() {
     return imdbVotes;
   }
 
+  /**
+   * Gets the genre(s) of the cinematic.
+   *
+   * @return The genre(s) as a comma-separated string.
+   */
   public String getGenre() {
     return genre;
   }
 
+  /**
+   * Sets the genre(s) of the cinematic.
+   *
+   * @param genre The genre(s) to set as a comma-separated string.
+   */
   public void setGenre(String genre) {
     this.genre = genre;
   }
 
+  /**
+   * Gets the title of the cinematic.
+   *
+   * @return The title of the cinematic.
+   */
   public String getTitle() {
     return title;
   }
 
+  /**
+   * Sets the title of the cinematic.
+   *
+   * @param title The title to set.
+   */
   public void setTitle(String title) {
     this.title = title;
   }
 
+  /**
+   * Gets the runtime of the cinematic.
+   *
+   * @return The runtime of the cinematic.
+   */
   public String getRuntime() {
     return runtime;
   }
 
+  /**
+   * Gets the director of the cinematic.
+   *
+   * @return The name of the director.
+   */
   public String getDirector() {
     return director;
   }
 
+  /**
+   * Gets the actors in the cinematic.
+   *
+   * @return A comma-separated string of actor names.
+   */
   public String getActors() {
     return actors;
   }
 
+  /**
+   * Gets the plot of the cinematic.
+   *
+   * @return The plot summary of the cinematic.
+   */
   public String getPlot() {
     return plot;
   }
 
+  /**
+   * Gets the URL of the poster image for the cinematic.
+   *
+   * @return The poster image URL.
+   */
   public String getPosterUrl() {
     return posterUrl;
   }
 
+  /**
+   * Gets the IMDb rating of the cinematic.
+   *
+   * @return The IMDb rating as a string.
+   */
   public String getImdbRating() {
     return imdbRating;
   }
