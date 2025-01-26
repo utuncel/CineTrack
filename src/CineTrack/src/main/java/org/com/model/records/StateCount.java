82dd5f599e222a83045037c15a9e734e717047bc
utuncel <umut.2000@t-online.de>
2025-01-26 14:00:56 +0100

    created comments records package

--- src/CineTrack/src/main/java/org/com/model/records/StateCount.java
+++ src/CineTrack/src/main/java/org/com/model/records/StateCount.java
@@ -1,5 +1,16 @@
 package org.com.model.records;
 
+/**
+ * A record representing the count of cinematics in different states.
+ *
+ * <p>This record is used to store the number of cinematics categorized by their
+ * current state, such as finished, dropped, currently watching, or planned to watch.</p>
+ *
+ * @param finished The count of finished cinematics.
+ * @param dropped  The count of cinematics that have been dropped.
+ * @param watching The count of cinematics currently being watched.
+ * @param towatch  The count of cinematics planned to be watched.
+ */
 public record StateCount(int finished, int dropped, int watching, int towatch) {
 
 }
--- src/CineTrack/src/main/java/org/com/model/records/TypeCount.java
+++ src/CineTrack/src/main/java/org/com/model/records/TypeCount.java
@@ -1,5 +1,15 @@
 package org.com.model.records;
 
+/**
+ * A record representing the count of different types of cinematics.
+ *
+ * <p>This record is used to store the number of movies, series, and anime
+ * for analytics or display purposes in the application.</p>
+ *
+ * @param movie  The count of movies.
+ * @param series The count of series.
+ * @param anime  The count of anime.
+ */
 public record TypeCount(int movie, int series, int anime) {
 
 }
