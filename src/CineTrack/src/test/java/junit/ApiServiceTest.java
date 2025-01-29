73542791f2e2c6b7585abf4fd2a437a4dec0bbce
utuncel <umut.2000@t-online.de>
2025-01-29 17:19:45 +0100

    implemented ApiServiceTests

--- src/CineTrack/src/test/java/junit/ApiServiceTest.java
+++ src/CineTrack/src/test/java/junit/ApiServiceTest.java
@@ -1,49 +1,42 @@
 package junit;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.List;
-import org.com.model.domain.CsvCinematic;
-import org.com.service.ApiService;
-import org.com.service.CineFactoryService;
-import org.com.service.CsvImporterService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.com.model.domain.ApiCinematic;
+import org.com.service.ApiService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ApiServiceTest {
 
-  private CineFactoryService cineFactoryService;
-  private CsvImporterService csvImporterService;
-  private File tempFile;
+  @InjectMocks
+  private ApiService apiService;
 
-  @BeforeEach
-  void setUp() throws IOException {
-    tempFile = File.createTempFile("Test", ".csv");
-    csvImporterService = new CsvImporterService(tempFile.getAbsolutePath());
+  @Before
+  public void setUp() {
+    System.setProperty("OMDb_API", "test_api_key");
   }
 
   @Test
-  void testReadCSVValid() throws IOException {
-    String validCSVContent = "Title,Type,State,Rating\n" +
-        "The Fighter,MOVIE,FINISHED,6\n" +
-        "Up,MOVIE,FINISHED,8\n" +
-        "The Curious Case of Benjamin Button,MOVIE,TOWATCH\n" +
-        "Oppenheimer,MOVIE,FINISHED,6\n";
+  public void testFetchMoviesOrSeries_SuccessfulResponse() {
 
-    writeToFile(validCSVContent);
+    ApiCinematic result = apiService.fetchMoviesOrSeries("Inception");
 
-    List<CsvCinematic> cinematics = csvImporterService.importData();
-    ApiService apiService = new ApiService();
-    for (CsvCinematic csvCinematic : cinematics) {
-      apiService.fetchMoviesOrSeries(csvCinematic.getTitle());
-    }
+    assertNotNull(result);
+    assertEquals("Inception", result.getTitle());
   }
 
-  private void writeToFile(String content) throws IOException {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
-      writer.write(content);
-    }
+  @Test
+  public void testFetchMoviesOrSeries_NotFound() {
+    ApiCinematic result = apiService.fetchMoviesOrSeries("Unknown Movie__1");
+
+    assertNull(result);
   }
 }
