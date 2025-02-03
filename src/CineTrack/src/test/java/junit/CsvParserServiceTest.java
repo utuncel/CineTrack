1ba73483ec4cd73726db75818b44e106a5cedd67
utuncel <umut.2000@t-online.de>
2025-02-03 18:23:55 +0100

    implemented CsvParserServiceTest amd LineValidatorServiceTest

---
+++ src/CineTrack/src/test/java/junit/CsvParserServiceTest.java
@@ -0,0 +1,76 @@
+package junit;
+
+import static org.junit.Assert.assertEquals;
+
+import javafx.application.Platform;
+import org.com.model.enums.State;
+import org.com.model.enums.Type;
+import org.com.service.CsvParserService;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CsvParserServiceTest {
+
+  @BeforeClass
+  public static void initJavaFX() {
+    Platform.startup(() -> {
+    });
+  }
+
+  @Test
+  public void testParseTypes_ValidType() {
+    CsvParserService parserService = new CsvParserService(1);
+    Type result = parserService.parseTypes("MOVIE");
+    assertEquals(Type.MOVIE, result);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testParseTypes_InvalidType() {
+    CsvParserService parserService = new CsvParserService(1);
+    parserService.parseTypes("INVALID_TYPE");
+  }
+
+  @Test
+  public void testParseStates_ValidState() {
+    CsvParserService parserService = new CsvParserService(1);
+    State result = parserService.parseStates("WATCHING");
+    assertEquals(State.WATCHING, result);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testParseStates_InvalidState() {
+    CsvParserService parserService = new CsvParserService(1);
+    parserService.parseStates("INVALID_STATE");
+  }
+
+  @Test
+  public void testParseStringToInt_ValidRating() {
+    CsvParserService parserService = new CsvParserService(1);
+    int result = parserService.parseStringToInt("5");
+    assertEquals(5, result);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testParseStringToInt_InvalidRatingLow() {
+    CsvParserService parserService = new CsvParserService(1);
+    parserService.parseStringToInt("0");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testParseStringToInt_InvalidRatingHigh() {
+    CsvParserService parserService = new CsvParserService(1);
+    parserService.parseStringToInt("11");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testParseStringToInt_NonNumericRating() {
+    CsvParserService parserService = new CsvParserService(1);
+    parserService.parseStringToInt("abc");
+  }
+
+  @Test
+  public void testDefaultConstructor() {
+    CsvParserService parserService = new CsvParserService();
+    // Verify no exceptions are thrown
+  }
+}
---
+++ src/CineTrack/src/test/java/junit/LineValidatorServiceTest.java
@@ -0,0 +1,67 @@
+package junit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javafx.application.Platform;
+import org.com.service.LineValidatorService;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LineValidatorServiceTest {
+
+  private LineValidatorService validatorService = new LineValidatorService();
+
+  @BeforeClass
+  public static void initJavaFX() {
+    Platform.startup(() -> {
+    });
+  }
+
+  @Test
+  public void testValidHeaderLength_Correct() {
+    assertTrue(validatorService.isValidHeaderLength("Title,Type,State,Rating"));
+  }
+
+  @Test
+  public void testValidHeaderLength_Incorrect() {
+    assertFalse(validatorService.isValidHeaderLength("Title,Type,State"));
+    assertFalse(validatorService.isValidHeaderLength("Title,Type,State,Rating,Extra"));
+  }
+
+  @Test
+  public void testValidHeader_Correct() {
+    assertTrue(validatorService.isValidHeader("Title,Type,State,Rating"));
+    assertTrue(validatorService.isValidHeader("title,type,state,rating"));
+  }
+
+  @Test
+  public void testValidHeader_Incorrect() {
+    assertFalse(validatorService.isValidHeader("Wrong,Headers,Here,Totally"));
+  }
+
+  @Test
+  public void testValidRecord_Correct() {
+    assertTrue(validatorService.isValidRecord(new String[]{"Inception", "MOVIE", "WATCHING"}));
+    assertTrue(validatorService.isValidRecord(new String[]{"Inception", "MOVIE", "WATCHING", "5"}));
+  }
+
+  @Test
+  public void testValidRecord_MissingRequiredFields() {
+    assertFalse(validatorService.isValidRecord(new String[]{"", "MOVIE", "WATCHING"}));
+    assertFalse(validatorService.isValidRecord(new String[]{"Inception", "", "WATCHING"}));
+    assertFalse(validatorService.isValidRecord(new String[]{"Inception", "MOVIE", ""}));
+  }
+
+  @Test
+  public void testValidRecord_BlankRating() {
+    assertFalse(validatorService.isValidRecord(new String[]{"Inception", "MOVIE", "WATCHING", ""}));
+  }
+
+  @Test
+  public void testValidRecord_InvalidColumnCount() {
+    assertFalse(validatorService.isValidRecord(new String[]{"Inception"}));
+    assertFalse(validatorService.isValidRecord(
+        new String[]{"Inception", "MOVIE", "WATCHING", "5", "Extra"}));
+  }
+}
